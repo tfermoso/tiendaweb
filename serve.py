1164b6d20e0c85e49cce5f4ee7e58db2fbac3f5a
cristianCmon <cristianXP@protonmail.com>
2022-02-17 16:47:59 +0100

    funcionalidad backend completada

--- serve.py
+++ serve.py
@@ -20,68 +20,68 @@ from jinja2 import Undefined
 def removeProduct(id):
     tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
+        tienda=json.load(file)
         file.close()
-    productos=tienda["productos"];
-    pEliminar=None;
+    productos=tienda["productos"]
+    pEliminar=None
     for item in productos:
         if(item["id"]==id):
-            pEliminar=item;
-            break;
-    productos.remove(pEliminar);
+            pEliminar=item
+            break
+    productos.remove(pEliminar)
     with open("dato_tienda.json",'w') as file:
-        json.dump(tienda,file,indent=2);
+        json.dump(tienda,file,indent=2)
         file.close()
-    return productos;
+    return productos
 
 def guardarProducto(nombreProducto,descripcion,filename,precio,cantidad):
     tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
+        tienda=json.load(file)
         file.close()
 
     if("contador" in tienda.keys()):
-        tienda["contador"]+=1;
+        tienda["contador"]+=1
     else:
-        tienda["contador"]=0;
+        tienda["contador"]=0
         
-    contador=tienda["contador"];
+    contador=tienda["contador"]
 
-    tienda["productos"].append(
-        {"nombre":nombreProducto,
+    tienda["productos"].append({
+        "nombre":nombreProducto,
         "descripcion":descripcion,
         "img":filename,
         "precio":precio,
         "cantidad":cantidad,
         "id":contador
-        })
+    })
 
     with open("dato_tienda.json",'w') as file:
-        json.dump(tienda,file,indent=2);
+        json.dump(tienda,file,indent=2)
         file.close()
-    return tienda["productos"];
+    return tienda["productos"]
 
 def nombreUnico(fichero,directorio):
     dir=pathlib.Path(directorio)
-    encontrado=False;
+    encontrado=False
     for file in dir.iterdir():
         if(file.name==fichero):
             encontrado=True
-            break;
+            break
     if(not encontrado):
         return fichero
     contador=fichero.split(".")[0][len(fichero.split(".")[0])-1]
     cont=0
     if(contador.isnumeric()):
-        cont=int(contador)+1;
+        cont=int(contador)+1
     fichero=fichero.split(".")[0]+str(cont)+"."+fichero.split(".")[1]
     return nombreUnico(fichero,directorio)
 
 def validar(usr,password):
-    tienda={};
+    tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
-        file.close();
+        tienda=json.load(file)
+        file.close()
     for u in tienda["usuarios"]:
         if(u["usuario"]==usr) and (u["password"]==hashlib.sha224(bytes(password,encoding="utf-8")).hexdigest()):
             return True
@@ -90,8 +90,8 @@ def validar(usr,password):
 def comprobarUsuario(usr):
     tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
-        file.close();
+        tienda=json.load(file)
+        file.close()
     for u in tienda["usuarios"]:
         if(u["usuario"]==usr):
             return True
@@ -100,21 +100,45 @@ def comprobarUsuario(usr):
 def registrarUsuario(usr,passw):
     tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
+        tienda=json.load(file)
         file.close()
 
     tienda["usuarios"].append({"usuario":usr,"password":hashlib.sha224(bytes(passw,encoding="utf-8")).hexdigest()})
 
     with open("dato_tienda.json",'w') as file:
-        json.dump(tienda,file,indent=2);
+        json.dump(tienda,file,indent=2)
         file.close()
 
 def leerProductosFichero():
     tienda={}
     with open("dato_tienda.json",'r') as file:
-        tienda=json.load(file);
+        tienda=json.load(file)
         file.close()
-    return tienda["productos"];
+    return tienda["productos"]
+
+
+def editProducto(idProducto, nombreProducto, descripcion, filename, precio, cantidad):
+
+    tienda={}
+
+    with open("dato_tienda.json",'r') as file:
+        tienda=json.load(file)
+        file.close()
+
+    for p in tienda["productos"]:
+        if p["id"] == idProducto:
+            p["nombre"] = nombreProducto
+            p["descripcion"] = descripcion
+            if filename != "":
+                p["img"] = filename
+            p["precio"] = precio
+            p["cantidad"] = cantidad
+            break
+        
+    with open("dato_tienda.json",'w') as file:
+        json.dump(tienda,file,indent=2)
+        file.close()
+            
 
 # creates a Flask application, named app
 app = Flask(__name__,static_folder='templates/static')
@@ -128,96 +152,112 @@ def hello():
 @app.route('/producto')
 def paginaProducto():
     #name = request.cookies.get('userId')
-    nombreUsuario=session.get("nombreUsuario");
+    nombreUsuario=session.get("nombreUsuario")
     print(nombreUsuario)
     if nombreUsuario==None:
-        return redirect(url_for('login'));
+        return redirect(url_for('login'))
     else:
      return render_template('index.html',nombre=nombreUsuario)
 
-@app.route('/crearproducto',methods=['GET','POST'])
+@app.route('/crearproducto', methods = ['GET', 'POST'])
 def crearProducto():
     if(request.method=='GET'):
         return render_template("formularioproducto.html")
     else:
-        nombreProducto=request.form["nombre"];
+        nombreProducto=request.form["nombre"]
         descripcion=request.form["descripcion"]
         f = request.files['archivo']    
-        filename = f.filename;
+        filename = f.filename
         filename=nombreUnico(filename,"./templates/static/img")
-        precio=request.form["precio"];
-        cantidad=request.form["cantidad"];
+        precio=request.form["precio"]
+        cantidad=request.form["cantidad"]
         f.save(os.path.join('./templates/static/img', filename))
         productos=guardarProducto(nombreProducto,descripcion,filename,precio,cantidad) 
-        print(productos);
-        return redirect(url_for("crearProducto"));
+        print(productos)
+        return redirect(url_for("crearProducto"))
 
 @app.route('/cerrarSesion')
 def cerrarSesion():
     response=make_response(redirect(url_for("login")))
-    response.delete_cookie("userId");
+    response.delete_cookie("userId")
     return response
 
 @app.route('/prueba')
 def prueba():
-    return "hola mundo";
+    return "hola mundo"
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
     if(request.method=="GET"):
         return render_template('login.html')
     elif  (request.method=="POST"):
-        usuario=request.form["usr"];
-        password=request.form["password"];
+        usuario=request.form["usr"]
+        password=request.form["password"]
         if(validar(usuario,password)):
             response = make_response(redirect(url_for('paginaProducto')))
-            #response.set_cookie("userId",hashlib.sha224(bytes(usuario,encoding="utf-8")).hexdigest());
-            session["nombreUsuario"]=usuario;
+            #response.set_cookie("userId",hashlib.sha224(bytes(usuario,encoding="utf-8")).hexdigest())
+            session["nombreUsuario"]=usuario
             return response
         else:
-            msg="Usuario o contraseña incorrecto";
+            msg="Usuario o contraseña incorrecto"
             return render_template('login.html',mensaje=msg)  
 
-@app.route('/registro',methods=['GET'])
+@app.route('/registro', methods = ['GET'])
 def registro():
-    return render_template("registro.html");
+    return render_template("registro.html")
 
-@app.route('/getproducts',methods=["GET"])
+@app.route('/getproducts', methods = ["GET"])
 def getProducts():
-    productos=leerProductosFichero();
-    return jsonify(productos);
+    productos=leerProductosFichero()
+    return jsonify(productos)
 
 
-@app.route('/crearUsuario',methods=['POST'])
+@app.route('/crearUsuario', methods = ['POST'])
 def crearUsuario():
-    usuario=request.form["nombre"];
-    password=request.form.get("password");
+    usuario=request.form["nombre"]
+    password=request.form.get("password")
     if(comprobarUsuario(usuario)):
-        msg="Usuario ya existe";
+        msg="Usuario ya existe"
         return redirect(url_for("registro",mensaje=msg))
     else: 
         #Crear usuario
         registrarUsuario(usuario,password)
         return redirect(url_for("login",usr=usuario))
 
-@app.route('/procesarLogin',methods=['POST'])
+@app.route('/procesarLogin', methods = ['POST'])
 def procesar():
     return "hola mundo"; 
 
 
-@app.route("/eliminarproducto",methods=["GET","POST"])
+@app.route("/eliminarproducto", methods = ["GET", "POST"])
 def eliminarProducto():
     idproducto=int(request.args["id"])
     result=removeProduct(idproducto)
-    return jsonify(result);
-    
+    return jsonify(result)
+
+
+@app.route("/editarproducto", methods = ["POST"])
+def editarProducto():
+    idProducto = int(request.form["idProducto"])
+    nombreProducto = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+    precio = request.form["precio"]
+    cantidad = request.form["cantidad"]
+    f = request.files["archivo"]    
+    filename = f.filename
+    if filename != "":
+        filename = nombreUnico(filename, "./templates/static/img")
+        f.save(os.path.join("./templates/static/img", filename))
+    productos = editProducto(idProducto, nombreProducto, descripcion, filename, precio, cantidad) 
+    #print(productos)
+    return redirect(url_for("crearProducto"))
 
 
 # run the application
 if __name__ == "__main__":
 
     app.secret_key = "generador de claves de sesión"
-    app.run(host="localhost",port=8085,debug=True)
+    app.run(host = "localhost", port = 8085, debug = True)
     
 
 
--- templates/static/js/productos.js
+++ templates/static/js/productos.js
@@ -1,42 +1,55 @@
-window.onload = function () {
 
+window.onload = function () {
+    // Declaración de variables
+    let productos = [];
+
+    // Funciones
     function cargarDatos() {
         ajax = new XMLHttpRequest();
+
         ajax.onreadystatechange = () => {
+
             if (ajax.readyState == 4) {
                 if (ajax.status == 200) {
                     productos = JSON.parse(ajax.responseText);
-
-                    mostrarProductos(productos)
+                    mostrarProductos(productos);
                 }
             } else {
-                console.log(ajax.readyState)
+                console.log(ajax.readyState);
             }
         }
         //ajax.open('GET', 'https://restcountries.com/v3.1/all', true);
         ajax.open('GET', 'http://localhost:8085/getproducts', true);
         ajax.send(null);
-
-
     }
+
 
     function mostrarProductos(productos) {
         let contenido = "";
         for (let i = 0; i < productos.length; i++) {
-            contenido += `<div class="filaProducto">
-            <img src="static/img/${productos[i].img}" alt="">
-            <div class="tituloProducto"><h4>${productos[i].nombre}</h4>
-            <p>${productos[i].descripcion}</p></div>
-            <div><label>Precio:</label><span>${productos[i].precio}</span><label>Cantidad:</label><span>${productos[i].cantidad}</span></div>
-            <div><img src="static/img/borrar.jpg" id="btnEliminar${productos[i].id}" class="btnBorrar" alt=""></div></div>`;
+            contenido += `
+                <div class="filaProducto">
+                    <img src="static/img/${productos[i].img}" alt="">
+                    <div class="tituloProducto">
+                        <h4>${productos[i].nombre}</h4>
+                        <p>${productos[i].descripcion}</p>
+                    </div>
+                    <div>
+                        <label>Precio:</label><span>${productos[i].precio}</span>
+                        <label>Cantidad:</label><span>${productos[i].cantidad}</span>
+                    </div>
+                    <div>
+                        <img src="static/img/borrar.jpg" id="btnEliminar${productos[i].id}" class="btnBorrar" alt="">
+                        <img src="static/img/editar.svg" id="btnEditar${productos[i].id}" class="btnEditar" alt="">
+                    </div>
+                </div>
+            `;
         }
         document.getElementById("listadoProductos").innerHTML = contenido;
         asociarEventos();
 
     }
-
-    setTimeout(cargarDatos, 3000);
-
+    setTimeout(cargarDatos, 1500);
 
 
     function asociarEventos() {
@@ -59,22 +72,48 @@ window.onload = function () {
                     }                               
                 }
                 */
-               ajax.onload=()=>{
-                productos = JSON.parse(ajax.responseText);
-                mostrarProductos(productos)
-               }
-               ajax.onerror=()=>{
-                   alert("Error en la petición");
-               }
+                ajax.onload = () => {
+                    productos = JSON.parse(ajax.responseText);
+                    mostrarProductos(productos);
+                }
+
+                ajax.onerror = () => {
+                    alert("Error en la petición");
+                }
+
                 let url = `http://localhost:8085/eliminarproducto?id=${idProducto}`;
                 //let url="http://localhost:8085/eliminarproducto?id="+idProducto;
                 //let url2="http://localhost:8085/eliminarproducto";
                 let params = "id=" + idProducto;
-                ajax.open("GET", url, true)
+                ajax.open("GET", url, true);
                 //ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                ajax.send(params)
+                ajax.send(params);
+            }
+        }
+
+        // Editar Productos
+        let lapices = document.getElementsByClassName("btnEditar");
+        for (let i = 0; i < lapices.length; i++) {
+            const elementoEditar = lapices[i];
+
+            elementoEditar.onclick = (evento) => {
+                
+                let idProducto = parseInt(evento.currentTarget.id.substring(9));
+                
+                productos.forEach(elemento => {
+                    if (elemento.id == idProducto) {
+                        document.getElementById("inputIdProducto").value = idProducto;
+                        document.getElementById("inputNombre").value = elemento.nombre;
+                        document.getElementById("inputDescripcion").value = elemento.descripcion;
+                        document.getElementById("inputPrecio").value = elemento.precio;
+                        document.getElementById("inputCantidad").value = elemento.cantidad;
+                        document.getElementById("inputArchivo").required = false;
+                        document.getElementById("inputSubmit").value = "Editar Producto";
+                        document.getElementById("inputSubmit").style.backgroundColor = "blue";
+                        document.forms[0].action = "http://localhost:8085/editarproducto";
+                    }
+                });
             }
         }
     }
-
 }

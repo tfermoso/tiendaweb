83b1037dbad1810c86d80ec0c4b67f6f4d8d7e74
HelsTR <el_che14_89_5@hotmail.com>
2022-02-17 16:14:20 +0100

    Edicion por Ajax

--- templates/static/js/productos.js
+++ templates/static/js/productos.js
@@ -28,7 +28,8 @@ window.onload = function () {
             <div class="tituloProducto"><h4>${productos[i].nombre}</h4>
             <p>${productos[i].descripcion}</p></div>
             <div><label>Precio:</label><span>${productos[i].precio}</span><label>Cantidad:</label><span>${productos[i].cantidad}</span></div>
-            <div><img src="static/img/borrar.jpg" id="btnEliminar${productos[i].id}" class="btnBorrar" alt=""></div></div>`;
+            <div><img src="static/img/borrar.jpg" id="btnEliminar${productos[i].id}" class="btnBorrar" alt=""></div>
+            <div><img src="static/img/btnEdit.png" id="btnEditar${productos[i].id}" class="btnEdit" alt=""></div></div>`;
         }
         document.getElementById("listadoProductos").innerHTML = contenido;
         asociarEventos();
@@ -75,6 +76,41 @@ window.onload = function () {
                 ajax.send(params)
             }
         }
+        let edit = document.getElementsByClassName("btnEditar");
+        for (let i = 0; i < edit.length; i++) {
+            const edicion = edit[i];
+            edicion.onclick = (evt) => {
+                let idProducto = evt.currentTarget.id.substring(9);
+                
+                ajax = new XMLHttpRequest();
+                /*
+                ajax.onreadystatechange = () => {
+                    if (ajax.readyState == 4){
+                        if (ajax.status == 200) {
+                            productos = JSON.parse(ajax.responseText);
+                            mostrarProductos(productos)
+                        }
+                        if(ajax.status==500){
+                            alert("Error en el Servidor")
+                        }
+                    }                               
+                }
+                */
+               ajax.onload=()=>{
+                productos = JSON.parse(ajax.responseText);
+                mostrarProductos(productos)
+               }
+               ajax.onerror=()=>{
+                   alert("Error en la petición");
+               }
+                let url = `http://localhost:8085/editarproducto?id=${idProducto}`;
+                //let url="http://localhost:8085/eliminarproducto?id="+idProducto;
+                //let url2="http://localhost:8085/eliminarproducto";
+                let params = "id=" + idProducto;
+                ajax.open("GET", url, true)
+                //ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                ajax.send(params)
+            }
+        }
     }
-
 }

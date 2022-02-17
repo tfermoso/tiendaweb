f160d948a26d660cddb3a88fc9dbf03e6a190cac
merilander <merilander@yahoo.es>
2022-02-17 17:16:25 +0100

    sdf

--- templates/static/js/productos.js
+++ templates/static/js/productos.js
@@ -1,80 +1,101 @@
 window.onload = function () {
 
-    function cargarDatos() {
-        ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = () => {
-            if (ajax.readyState == 4) {
-                if (ajax.status == 200) {
-                    productos = JSON.parse(ajax.responseText);
+  function cargarDatos() {
+    ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = () => {
+      if (ajax.readyState == 4) {
+        if (ajax.status == 200) {
+          productos = JSON.parse(ajax.responseText);
 
-                    mostrarProductos(productos)
-                }
-            } else {
-                console.log(ajax.readyState)
-            }
+          mostrarProductos(productos)
         }
-        //ajax.open('GET', 'https://restcountries.com/v3.1/all', true);
-        ajax.open('GET', 'http://localhost:8085/getproducts', true);
-        ajax.send(null);
-
-
+      } else {
+        console.log(ajax.readyState)
+      }
     }
+    //ajax.open('GET', 'https://restcountries.com/v3.1/all', true);
+    ajax.open('GET', 'http://localhost:8085/getproducts', true);
+    ajax.send(null);
 
-    function mostrarProductos(productos) {
-        let contenido = "";
-        for (let i = 0; i < productos.length; i++) {
-            contenido += `<div class="filaProducto">
+
+  }
+
+  function mostrarProductos(productos) {
+    let contenido = "";
+    for (let i = 0; i < productos.length; i++) {
+      contenido += `<div class="filaProducto">
             <img src="static/img/${productos[i].img}" alt="">
             <div class="tituloProducto"><h4>${productos[i].nombre}</h4>
             <p>${productos[i].descripcion}</p></div>
             <div><label>Precio:</label><span>${productos[i].precio}</span><label>Cantidad:</label><span>${productos[i].cantidad}</span></div>
             <div><img src="static/img/borrar.jpg" id="btnEliminar${productos[i].id}" class="btnBorrar" alt=""></div></div>`;
-        }
-        document.getElementById("listadoProductos").innerHTML = contenido;
-        asociarEventos();
-
     }
+    document.getElementById("listadoProductos").innerHTML = contenido;
+    asociarEventos();
 
-    setTimeout(cargarDatos, 3000);
+  }
+
+  setTimeout(cargarDatos, 3000);
 
 
 
-    function asociarEventos() {
-        let papeleras = document.getElementsByClassName("btnBorrar");
-        for (let i = 0; i < papeleras.length; i++) {
-            const papelera = papeleras[i];
-            papelera.onclick = (evt) => {
-                let idProducto = evt.currentTarget.id.substring(11);
-                ajax = new XMLHttpRequest();
-                /*
-                ajax.onreadystatechange = () => {
-                    if (ajax.readyState == 4){
-                        if (ajax.status == 200) {
-                            productos = JSON.parse(ajax.responseText);
-                            mostrarProductos(productos)
-                        }
-                        if(ajax.status==500){
-                            alert("Error en el Servidor")
-                        }
-                    }                               
+  function asociarEventos() {
+    let papeleras = document.getElementsByClassName("btnBorrar");
+    for (let i = 0; i < papeleras.length; i++) {
+      const papelera = papeleras[i];
+      papelera.onclick = (evt) => {
+        let idProducto = evt.currentTarget.id.substring(11);
+        ajax = new XMLHttpRequest();
+        /*
+        ajax.onreadystatechange = () => {
+            if (ajax.readyState == 4){
+                if (ajax.status == 200) {
+                    productos = JSON.parse(ajax.responseText);
+                    mostrarProductos(productos)
                 }
-                */
-               ajax.onload=()=>{
-                productos = JSON.parse(ajax.responseText);
-                mostrarProductos(productos)
-               }
-               ajax.onerror=()=>{
-                   alert("Error en la petición");
-               }
-                let url = `http://localhost:8085/eliminarproducto?id=${idProducto}`;
-                //let url="http://localhost:8085/eliminarproducto?id="+idProducto;
-                //let url2="http://localhost:8085/eliminarproducto";
-                let params = "id=" + idProducto;
-                ajax.open("GET", url, true)
-                //ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                ajax.send(params)
-            }
+                if(ajax.status==500){
+                    alert("Error en el Servidor")
+                }
+            }                               
         }
+        */
+        ajax.onload = () => {
+          productos = JSON.parse(ajax.responseText);
+          mostrarProductos(productos)
+        }
+        ajax.onerror = () => {
+          alert("Error en la petición");
+        }
+        let url = `http://localhost:8085/eliminarproducto?id=${idProducto}`;
+        //let url="http://localhost:8085/eliminarproducto?id="+idProducto;
+        //let url2="http://localhost:8085/eliminarproducto";
+        let params = "id=" + idProducto;
+        ajax.open("GET", url, true)
+        //ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        ajax.send(params)
+      }
     }
+  }
 
+  //Editar producto
+  let lapices = document.getElementsByClassName("btnEditar");
+  for (let i = 0; i < lapices.length; i++) {
+    const elementEdit = lapices[i];
+    elementEdit.onclick = (evt) => {
+      let idProducto = parseInt(evt.currentTarget.id.substring(9));
+      productos.forEach((element) => {
+        if (element.id == idProducto) {
+          document.getElementById("inputIdProducto").value = idProducto;
+          document.getElementById("inputNombre").value = element.nombre;
+          document.getElementById("inputDescripcion").value =
+            element.descripcicon;
+          document.getElementById("inputPrecio").value = element.precio;
+          document.getElementById("inputCantidad").value = element.cantidad;
+          document.getElementById("inputArchivo").required = false;
+          document.getElementById("inputSubmit").value = "Editar Producto";
+          document.forms[0].action = "http://localhost:8085/editarproducto";
+        }
+      });
+    };
+  }
 }
